7a53a6a91a70a1ce1d418b6667560539c75e855c
Vipul Jadav <vipulj@kaushalaminc.com>
2025-08-12 16:22:48 +0530

    worked on import dependency

--- app/Modules/UploadGradeCDI/routes/api_old.php
+++ app/Modules/UploadGradeCDI/routes/api_old.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 
 Route::group(['module' => 'Submissions', 'middleware' => ['api'], 'namespace' => 'App\Modules\Submissions\Controllers'], function() {
 
